999abed4e7154d7942c74fdae733efaa55ffd50b
Suraj Gund <surajgund46@gmail.com>
2024-12-23 06:57:24 +0530

    All about String

--- Basics/Conversion.js
+++ Basics/Conversion.js
@@ -1,5 +1,39 @@
 let score = 33;
 let string = String(score);
-console.log(typeof string);
+console.log(typeof string);//String
 let a = 123;
-console.log(null === undefined);
+console.log(null === undefined);//false
+console.log(+true);
+console.log(+null);
+console.log(+undefined);
+console.log(a++,++a);
+if(console.log(console.log("true"))){
+    console.log("True")
+}else{
+    console.log("False");
+}
+
+console.log(null != undefined);
+console.log(null !== undefined);
+
+console.log(NaN == NaN);
+console.log(NaN === NaN);
+function abcd(a){
+    console.log(a);
+    a = "def";
+}
+a = "abc";
+abcd(a);
+console.log(a);
+const isLogged = true;
+//isLogged = false;
+const id = Symbol('123');
+const id2 = Symbol('123');
+console.log(id,id2);
+const obj = {
+    name:'suraj',
+    myFunction:()=>{
+        console.log("My Function");
+    }
+}
+console.log(typeof obj.myFunction);
--- Basics/String.js
+++ Basics/String.js
@@ -1,20 +1,24 @@
-let obj = {
-    name:"Suraj",
-    canFly:false,
-    canTalk:true,
-}
+// let obj = {
+//     name:"Suraj",
+//     canFly:false,
+//     canTalk:true,
+// }
 
-let coder = {
-    canCode:true,
-    canSolveDSA:true
-}
+// let coder = {
+//     canCode:true,
+//     canSolveDSA:true
+// }
 
-coder.__proto__ = obj;
-let a = 10;
-function abcd(){
-    let a = 12;
-    return a;
-}
-console.log(a);
-a = abcd();
-console.log(a);
+// coder.__proto__ = obj;
+// let a = 10;
+// function abcd(){
+//     let a = 12;
+//     return a;
+// }
+// console.log(a);
+// a = abcd();
+// console.log(a);
+
+const string = "suraj-Gund";
+console.log(string.slice(0));
+console.log(string.replace('-',' '));
