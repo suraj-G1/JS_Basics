41191fb3021bf7b7707cb51393f95793da426f7f
Suraj Gund <surajgund46@gmail.com>
2025-03-20 06:47:29 +0530

    Revision Basics

--- Basics/DataType.js
+++ Basics/DataType.js
@@ -2,3 +2,8 @@ const accountId = "Suraj123";
 let email = "surajgund46";
 var pass = '123456';
 city = 'Solapur';
+console.log(suraj);
+console.log(gund);//will give "ReferenceError : Can't access variable before initializaiton"
+var suraj;
+let gund;
+//const krish;//Const must be initialized
